a466188bb85bc85d35433cf5762e4d0ff1ea90b1
Michelle Wang <wang.michellej@gmail.com>
2014-05-26 01:46:17 -0400

    added some new routes and html pages

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var body_parser = require('body-parser');
+
 
 module.exports = function config(app){
 
   app.use("/css", express.static(__dirname + "/css"));
   app.use("/views", express.static(__dirname + "/views"));
 
-
+  app.use(body_parser());
 }
 
--- routes.js
+++ routes.js
@@ -10,4 +10,19 @@ module.exports = function routes(app){
 	app.get('/test', function(req, res) {
 		console.log('Testing GamEdu');
 	});
+
+	app.get('/student_home', function(req, res){
+		res.sendfile(__dirname + '/views/student_home.html');
+	});
+	
+	app.route('/login')
+	  .get(function(req, res, next){
+		res.sendfile(__dirname + '/views/login.html');
+	})
+	  .post(function(req, res, next){
+		var pw = req.body.password;
+		console.log(pw);
+		res.sendfile(__dirname + '/views/teacher_home.html');
+	})
+	
 }
